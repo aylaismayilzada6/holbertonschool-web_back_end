828c35f0324d79172d47f2f429603e8df1608d1c
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-06-21 13:23:59 +0000

    task

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Display stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(f"{collection.count_documents({})} logs")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {count}")
+    status = collection.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{status} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
